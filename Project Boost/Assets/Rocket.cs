443ef15d868356164b4839ad00d98ff3a72655a0
MATTHEW-LAPTOP\Matthew <matthew08221996>
2018-04-27 13:21:25 -0400

    Added sounds to the rocket ( there is an unusual delay)

--- Project Boost/Assets/Rocket.cs
+++ Project Boost/Assets/Rocket.cs
@@ -6,17 +6,19 @@ using UnityEngine;
 public class Rocket : MonoBehaviour
 {
     Rigidbody rigidBody;
-    
-	// Use this for initialization
+    AudioSource RocketSound;
+   	// Use this for initialization
 	void Start ()
     {
         rigidBody = GetComponent<Rigidbody>();
+        RocketSound = GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
         ProcessInput();
+        
 	}
 
     private void ProcessInput()
@@ -24,15 +26,25 @@ public class Rocket : MonoBehaviour
         if (Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.W))
         {
             rigidBody.AddRelativeForce(Vector3.up);
+            
+            if(!RocketSound.isPlaying)
+            {
+                RocketSound.Play();
+            }
+
+        }
+        else
+        {
+            RocketSound.Stop();
         }
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Rotate(Vector3.forward);
+            transform.Rotate(Vector3.forward * 2);
         }
         else if (Input.GetKey(KeyCode.D))
         {
 
-            transform.Rotate(-Vector3.forward);
+            transform.Rotate(-Vector3.forward * 2);
         }
 
 
